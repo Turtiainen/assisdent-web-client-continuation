8dcc61337e25c8268d6d16e58a970131e1d46b6b
Juubes <grumpt1@gmail.com>
2023-02-16 19:15:48 +0200

    templates using tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { OrganizationName } from './OrganizationName';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Dropdown from './components/Dropdown';
 
 function App() {
     const queryClient = new QueryClient();
@@ -10,6 +11,13 @@ function App() {
             <div className="App">
                 <h1>Proto2</h1>
                 <OrganizationName />
+
+                <Dropdown
+                    // TODO: testipalikka, poistoon
+                    label="Dropdown"
+                    options={['Option 1', 'Option 2', 'Option 3']}
+                    onChange={(value) => console.log(value)}
+                ></Dropdown>
             </div>
         </QueryClientProvider>
     );
---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,58 @@
+export type DropdownProps = {
+    label: string;
+    options: string[];
+
+    // Optional props
+    value?: string;
+    disabled?: boolean;
+    error?: string;
+    placeholder?: string;
+    className?: string;
+    onChange: (value: string) => void;
+};
+
+function Dropdown(props: DropdownProps) {
+    let {
+        label,
+        options,
+        // Optional props
+        onChange,
+        disabled,
+        placeholder,
+        className,
+        value,
+        error,
+    } = props;
+
+    return (
+        <div className={className}>
+            <label className="block text-sm font-medium text-gray-700">
+                {label}
+            </label>
+            <select
+                className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+                onChange={(e) => onChange(e.target.value)}
+                disabled={disabled}
+                value={value}
+            >
+                {placeholder && (
+                    <option value="" disabled>
+                        {placeholder}
+                    </option>
+                )}
+                {options.map((option) => (
+                    <option key={option} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+            {error && (
+                <p className="mt-2 text-sm text-red-600" id="email-error">
+                    {error}
+                </p>
+            )}
+        </div>
+    );
+}
+
+export default Dropdown;
