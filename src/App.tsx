567489f41f9c8ea5bd40ae06d8d0a9c8089d26ac
Juubes <grumpt1@gmail.com>
2023-02-18 23:26:04 +0200

    Simple input done

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,21 @@ import { OrganizationName } from './OrganizationName';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { Sidebar } from './components/Sidebar';
+import Input from './components/Input';
 
 function App() {
     const queryClient = new QueryClient();
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <Sidebar />
-        <h1>Proto2</h1>
-          <OrganizationName />
-      </div>
-    </QueryClientProvider>
-  )
+    return (
+        <QueryClientProvider client={queryClient}>
+            <div className="App">
+                <Sidebar />
+                <h1>Proto2</h1>
+                <OrganizationName />
+            </div>
+
+        </QueryClientProvider>
+    );
 }
 
 export default App;
---
+++ src/components/Input.tsx
@@ -0,0 +1,31 @@
+export type InputProps = {
+    label: string;
+    placeholder?: string;
+    className?: string;
+
+    onChange?: (value: string) => void;
+    error?: string;
+};
+
+function Input(props: InputProps) {
+    const { className, label, onChange, error } = props;
+
+    return (
+        <div className={'flex w-full' + className}>
+            <label className="text-left m-auto block w-1/2 text-sm font-medium text-gray-700">
+                {label}
+            </label>
+            <input
+                className="mt-1 block w-1/2  pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+                onChange={onChange && ((e) => onChange(e.target.value))}
+            ></input>
+            {error && (
+                <p className="mt-2 text-sm text-red-600" id="email-error">
+                    {error}
+                </p>
+            )}
+        </div>
+    );
+}
+
+export default Input;
