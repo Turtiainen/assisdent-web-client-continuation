626830e00067b5ee277b60d89d459c41ca22792e
Markku Nirkkonen <markku.nirkkonen@tuni.fi>
2023-03-17 13:20:23 +0200

    Lint errors fixed on all files

--- src/components/View/ViewHeader.tsx
+++ src/components/View/ViewHeader.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React from 'react';
 
-export const ViewHeader = ({heading}: {heading: string}) => {
-  return (
-    <header className={`w-full bg-white p-8 mb-4`}>
-      <h1 className={`text-3xl text-ad-hero-title font-medium`}>{heading}</h1>
-    </header>
-  )
-}
+export const ViewHeader = ({ heading }: { heading: string }) => {
+    return (
+        <header className={`w-full bg-white p-8 mb-4`}>
+            <h1 className={`text-3xl text-ad-hero-title font-medium`}>
+                {heading}
+            </h1>
+        </header>
+    );
+};
