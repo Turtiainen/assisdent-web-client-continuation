e011d29031286b5ae5fe5d66c97fba47b30ce3ec
Johannes Maatta <johannes.maatta@tuni.fi>
2023-04-06 10:09:19 +0300

    Update badge props

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,9 +1,12 @@
 import { ReactElement, JSXElementConstructor, ReactFragment, ReactPortal } from "react"
-import "./Badge.css"
 
-export const Badge = (props: { isActive: any; title: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined }) => {
-    return <h2 className={
+type badgeProps = { isActive: any; title: string | number | boolean 
+  | ReactElement<any, string | JSXElementConstructor<any>> 
+  | ReactFragment | ReactPortal | null | undefined }
+
+export const Badge = (props: badgeProps) => {
+    return <div className={
       "header" +
       (props.isActive ? " active" : "")
-    }>{props.title}</h2>
+    }>{props.title}</div>
   }
