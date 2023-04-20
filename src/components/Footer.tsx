fb5b96dedec0f53f7a50f9aa833281f7585e82bc
Petri Kreus <anxored@gmail.com>
2023-04-20 16:06:10 +0300

    Generalize Footer and Button components

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,24 @@
+import { createPortal } from 'react-dom';
+
 type footerProps = {
     children: React.ReactNode;
+    className?: string;
 };
+
+const mainElement = document.querySelector('main');
 
 export const Footer = (props: footerProps) => {
     return (
-        <div
-            className={`toolbar bg-white border-t-2 border-ad-blue-600 flex justify-between fixed bottom-px px-4 w-full`}
-        >
-            <div className={`page-actions`}>
-                <span>{props.children}</span>
-            </div>
-            <div className="current-page"></div>
-            <div className="windows-actions"></div>
-        </div>
+        mainElement &&
+        createPortal(
+            <div
+                className={`toolbar bg-white border-t-2 border-ad-blue-600 absolute bottom-px px-4 ${
+                    props.className && props.className
+                }`}
+            >
+                {props.children}
+            </div>,
+            mainElement,
+        )
     );
 };
