060c5f9a2e8fa0e5a1165dcf8e913b78325fdf9a
Petri Kreus <anxored@gmail.com>
2023-04-20 16:39:57 +0300

    Fix Badge styles

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,20 +1,26 @@
-import {
-    ReactElement,
-    JSXElementConstructor,
-    ReactFragment,
-    ReactPortal,
-} from 'react';
+import React from 'react';
 
 type badgeProps = {
-    isInactive?: boolean;
+    isActive?: boolean;
     children: React.ReactNode;
+    className?: string;
+    onClick?: () => void;
 };
 
 export const Badge = (props: badgeProps) => {
+    const baseStyles = `select-none p-1 rounded-full w-6 h-6 text-sm flex justify-center items-center font-semibold`;
+    const transitionStyles = `transition-[background-color] duration-[300ms] ease-out`;
+
     return (
-        <span className={`${props.isInactive ? 'bg-ad-grey-400 hover:bg-ad-grey-200' : 
-        'text-white bg-ad-primary hover:bg-ad-primary-hover active:bg-ad-primary-pressed'} p-1 rounded-full w-6 h-6 inline-block text-xs text-center`}>
+        <button
+            className={`${baseStyles} ${transitionStyles} ${
+                props.isActive
+                    ? 'text-white bg-ad-primary hover:bg-ad-primary-hover active:bg-ad-primary-pressed'
+                    : 'hover:bg-ad-grey-200 active:bg-ad-grey-300 cursor-pointer'
+            } ${props.className}`}
+            onClick={props.onClick || undefined}
+        >
             {props.children}
-        </span>
+        </button>
     );
 };
