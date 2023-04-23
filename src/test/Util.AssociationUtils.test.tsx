1bc8e85d05a251bb2823b2c1367a9400bb6dcaa7
Markku Nirkkonen <markku.nirkkonen@tuni.fi>
2023-04-24 00:13:17 +0300

    few new tests

--- src/test/Util.AssociationUtils.test.tsx
+++ src/test/Util.AssociationUtils.test.tsx
@@ -2,6 +2,7 @@ import '@testing-library/jest-dom';
 import {
     commonFieldsReducer,
     getAssociationType,
+    mapAssociationTypeAddNewPatchCommands,
     mapAssociationTypeUpdatePatchCommands,
 } from '../utils/associationUtils';
 import { DynamicObject } from '../types/DynamicObject';
@@ -39,7 +40,7 @@ describe('Call getAssociationType', () => {
     });
 });
 
-describe('Call mapAssociationTypePatchCommands', () => {
+describe('Call mapAssociationTypeUpdatePatchCommands', () => {
     it('should return empty array when called with such', () => {
         const returnValue = mapAssociationTypeUpdatePatchCommands([{}]);
         expect(returnValue).toEqual([{}]);
@@ -101,6 +102,78 @@ describe('Call mapAssociationTypePatchCommands', () => {
             {
                 test: {
                     _update: [
+                        {
+                            testKey: 'testValue',
+                        },
+                    ],
+                },
+            },
+        ]);
+    });
+});
+
+describe('Call mapAssociationTypeAddNewPatchCommands', () => {
+    it('should return empty array when called with such', () => {
+        const returnValue = mapAssociationTypeAddNewPatchCommands([{}]);
+        expect(returnValue).toEqual([{}]);
+    });
+
+    it('should remove associationType field when called with such object in array', () => {
+        const returnValue = mapAssociationTypeAddNewPatchCommands([
+            {
+                associationType: 'Aggregation',
+            },
+        ]);
+        expect(returnValue).toEqual([{}]);
+    });
+
+    it('should leave other fields untouched', () => {
+        const returnValue = mapAssociationTypeAddNewPatchCommands([
+            {
+                associationType: null,
+                test: 'test',
+            },
+        ]);
+        expect(returnValue).toEqual([{ test: 'test' }]);
+    });
+
+    it('should work similarly with two objects in array', () => {
+        const returnValue = mapAssociationTypeAddNewPatchCommands([
+            {
+                associationType: null,
+                test: 'test',
+            },
+            {
+                associationType: null,
+                test2: 'test2',
+            },
+        ]);
+        expect(returnValue).toEqual([{ test: 'test' }, { test2: 'test2' }]);
+    });
+
+    it('should add _set_ref level to object with Aggregation associationType between the other key and its value', () => {
+        const returnValue = mapAssociationTypeAddNewPatchCommands([
+            {
+                associationType: AssociationType.Aggregation,
+                test: 'test',
+            },
+        ]);
+        expect(returnValue).toEqual([{ test: { _add_ref: ['test'] } }]);
+    });
+
+    it('should work similarly for Compositions but by adding _update level', () => {
+        const returnValue = mapAssociationTypeAddNewPatchCommands([
+            {
+                associationType: AssociationType.Composition,
+                test: {
+                    testKey: 'testValue',
+                },
+            },
+        ]);
+        expect(returnValue).toEqual([
+            {
+                test: {
+                    _create: [
                         {
                             testKey: 'testValue',
                         },
