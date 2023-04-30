62374b5ae6b52ed4ff31a659f8384235e86370f4
Petri Kreus <anxored@gmail.com>
2023-04-30 19:50:08 +0300

    Fix tests by using inline svg element in component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,6 +1,5 @@
 import { InputHTMLAttributes } from 'react';
 import { Label } from './Label';
-import InputTypeImage from './InputTypeImage.svg';
 
 type Input = {
     labelText?: string;
@@ -15,7 +14,31 @@ export const Input = ({ labelText, labelClassName, ...rest }: Input) => {
             </Label>
 
             {rest.type === 'Image' ? (
-                <InputTypeImage />
+                <svg
+                    height="34"
+                    viewBox="0 0 21 21"
+                    width="34"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <g
+                        fill="blue"
+                        fillRule="evenodd"
+                        stroke="white"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        transform="translate(2 2)"
+                    >
+                        <circle cx="8.5" cy="8.5" r="8" />
+                        <path
+                            d="m14.5 13.5c-.6615287-2.2735217-3.1995581-3.0251263-6-3.0251263-2.72749327 0-5.27073171.8688092-6 3.0251263"
+                            fill="white"
+                        />
+                        <path
+                            d="m8.5 2.5c1.6568542 0 3 1.34314575 3 3v2c0 1.65685425-1.3431458 3-3 3-1.65685425 0-3-1.34314575-3-3v-2c0-1.65685425 1.34314575-3 3-3z"
+                            fill="white"
+                        />
+                    </g>
+                </svg>
             ) : (
                 <input
                     {...rest}
