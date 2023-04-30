df705153b67f8a5acdaccd25521cc1644700c27c
Petri Kreus <anxored@gmail.com>
2023-04-30 19:43:03 +0300

    Fix linting errors

--- src/components/InputRow.tsx
+++ src/components/InputRow.tsx
@@ -1,10 +1,11 @@
 import { HTMLAttributes } from 'react';
 
-type InputRow = {} & HTMLAttributes<HTMLDivElement>;
+type InputRow = HTMLAttributes<HTMLDivElement>;
 
 export const InputRow = ({ className, children, ...rest }: InputRow) => {
     return (
         <div
+            {...rest}
             className={`flex flex-col xl:flex-row mb-4 ${
                 className ? className : ''
             }`}
